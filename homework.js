cc5920109a4cf323d6ab3a0be3bf07db7637b3f1
Tran Thien Tuong <tranthientuong.127@gmail.com>
2022-08-08 16:55:00 +0700

    homeworklab10

---
+++ homework.js
@@ -0,0 +1,104 @@
+/**
+ * Tính chu vi hình chữ nhật
+ *
+ * @param {number} length Chiều dài hình chữ nhật
+ * @param {number} breadth Chiều rộng hình chữ nhật
+ *
+ * @return {number} Chu vi hình chữ nhật
+ */
+function findPerimeterOfRectangle(length, breadth) {}
+
+function findPerimeterOfRectangle(a, b) {
+  return (a + b) * 2;
+}
+console.log(findPerimeterOfRectangle(2, 3));
+
+/**
+ * Tính diện tích hình chữ nhật
+ *
+ * @param {number} length Chiều dài hình chữ nhật
+ * @param {number} breadth Chiều rộng hình chữ nhật
+ *
+ * @return {number} Diện tích hình chữ nhật
+ */
+function findAreaOfRectangle(length, breadth) {}
+
+function findAreaOfRectangle(a, b) {
+  return a * b;
+}
+console.log(findAreaOfRectangle(2, 3));
+
+/**
+ * Tìm đường kính hình tròn
+ *
+ * @param {number} r Bán hình hình tròn
+ *
+ * @return {number} Đường kính hình tròn
+ */
+function findDiameterOfCircle(r) {}
+
+function findDiameterOfCircle(a) {
+  return a * 2;
+}
+console.log(findDiameterOfCircle(2));
+
+/**
+ * Tìm chu vi hình tròn
+ *
+ * Quy ước PI = 3.14
+ *
+ * @param {number} r Bán hình hình tròn
+ *
+ * @return {number} Chu vi hình tròn
+ */
+function findCircumferenceOfCircle(r) {
+  return 3.14 * 2 * r;
+}
+console.log(findCircumferenceOfCircle(3));
+/**
+ * Tìm diện tích hình tròn
+ *
+ * Quy ước PI = 3.14
+ *
+ * Bình phương sử dụng toán tử ** hoặc Math.pow()
+ *
+ * @param {number} r Bán kính hình tròn
+ *
+ * @return {number} Diện tích hình tròn
+ */
+function findAreaOfCircle(r) {
+  return 3.14 * r * r;
+}
+console.log(findAreaOfCircle(2));
+
+/**
+ * Tính diện tích hình tam giác theo công thức Heron
+ *
+ * Link tham khảo công thức Heron: https://vi.wikipedia.org/wiki/C%C3%B4ng_th%E1%BB%A9c_Heron
+ *
+ * @param {number} a Chiều dài cạnh a
+ * @param {number} b Chiều dài cạnh b
+ * @param {number} c Chiều dài cạnh c
+ *
+ * @return {number} Diện tích hình tam giác
+ */
+function findAreaOfTriangle(a, b, c) {
+  return Math.sqrt((a + b + c) * (a + b - c) * (b + c - a) * (c + a - b)) / 4;
+}
+console.log(findAreaOfTriangle(6, 8, 10));
+
+/**
+ * Chuyển đổi nhiệt ở ở thang nhiệt Celsius sang Fahrenheit
+ *
+ * Tham khảo công thức chuyển đổi trên google
+ *
+ * @param {number} temp Nhiệt độ ở thang nhiệt Celsius
+ *
+ * @return {number} Nhiệt độ ở thang nhiệt Fahrenheit
+ */
+function celsiusToFahrenheit(temp) {}
+
+function celsiusToFahrenheit(a) {
+  return a * 1.8 + 32;
+}
+console.log(celsiusToFahrenheit(1));
